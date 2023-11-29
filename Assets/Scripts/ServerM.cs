a6d737991cbfd1c1a106537b91b260a5670d8aef
quangnd-3189 <nguyen.duc.quang@sun-asterisk.com>
2023-11-29 15:13:15 +0700

    init test build ds

---
+++ Assets/Scripts/ServerM.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Server is running");
+    }
+
+
+}
